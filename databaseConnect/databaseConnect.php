279ab081ce831d5d00109243804fc0bcb35b66be
Soma az Isten <117622501+Somalol@users.noreply.github.com>
2024-10-02 18:19:48 +0200

    olvass readme-t

---
+++ databaseConnect/databaseConnect.php
@@ -0,0 +1,35 @@
+<?php
+    function adatLekeres($muvelet)
+    {
+        $db = new mysqli("localhost", "root", "", "vizsgaremek_suliujsag");
+        
+        if($db->connect_errno == 0)
+        {
+            $eredmeny = $db->query($muvelet);
+
+            if($db->errno == 0)
+            {
+                if($eredmeny->num_rows != 0)
+                {
+                    $adatok = $eredmeny->fetch_all(MYSQLI_ASSOC);
+                    return $adatok;
+                }
+
+                else
+                {
+                    return "Nincsenek találatok!";
+                }
+            }
+
+            else
+            {
+                return $db->error;
+            }
+        }
+
+        else 
+        {
+            return $db->connect_error;
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,10 +1,27 @@
+<?php
+    include "./databaseConnect/databaseConnect.php";
+?>
+
 <html lang="hu">
 <head>
+    <!--"meta" tagek-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!--Bootstrap 5 CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!--Saját CSS-->
+    <link rel="stylesheet" href="./css/index.css">
+
+    <!--"title" tag-->
     <title>Suliújság</title>
 </head>
 <body>
+    <!--Bootstrap 5 JavaScript Bundle-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+    <!--Saját JavaScript-->
+    <script src="./js/index.js"></script>
 </body>
 </html>
