1572aa20d8bbbadd06e672f3363f33a8d6da0ba9
Somalol <tivsoma@gmail.com>
2025-04-04 11:52:38 +0200

    sok teszt kész, de monoton és meguntam :((((((

--- tests/Feature/DiakokTest.php
+++ tests/Feature/DiakokTest.php
@@ -8,11 +8,45 @@ use Tests\TestCase;
 
 class DiakokTest extends TestCase
 {
-    public function test_example(): void
+    public function test_api_diakok(): void
     {
+        //Összes diák lekérése
         $response = $this
             ->get('/api/diakok');
 
-        $response->assertStatus(200);
+        $this->assertNotEmpty($response->getContent());
+    }
+
+
+    public function test_api_diakIdAlapjan(): void
+    {
+        //Egy diák lekérése id alapján (id: 18 --> Gipsz Jakab)
+        $diakAdat = [
+            "id" => 18
+        ];
+
+        $response = $this
+            ->post("/api/diakIdAlapjan", $diakAdat);
+
+        $response->assertJson([["nev" => "Gipsz Jakab"]]);
+    }
+
+
+    public function test_api_register_diak(): void 
+    {
+        $ujDiakAdatai = [
+            "email" => "teszt@gmail.com",
+            "teljesNev" => "Teszt Sándor",
+            "felhasznalonev" => "Teszter",
+            "iskola" => 1,
+            "osztaly" => 1,
+            "evfolyam" => 9,
+            "jelszo" => "1"
+        ];
+
+        $response = $this
+            ->post("/api/registerdiak", $ujDiakAdatai);
+
+        $response->assertStatus(201);
     }
 }
